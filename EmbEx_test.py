83ef0312913d9a66b2a78572d3edc7e816b8e6ee
ahhodson <hodson.alexander@outlook.com>
2026-08-09 16:56:30 +0100

    Add files via upload

---
+++ EmbEx_test.py
@@ -0,0 +1,125 @@
+from EmbExSim2 import *
+import numpy as np
+from copy import deepcopy
+from tools import sort_np_array_rows_lexicographically as canon
+
+class UI():
+
+    def __init__(self, n ,k):
+        self.n = n
+        self.k = k
+        self.codec = EmbExSim2(n,k)
+
+    def menu(self):
+        input_dict = {1: (self.specific_array, "specific array"), 2: (self.random_arrays, "random arrays")}
+        last = max(list(input_dict.keys()))
+        close = False
+        while not close:
+            print(f"Codec for n={self.n}, k={self.k}")
+            print("0: Exit")
+            for key in input_dict.keys():
+                print(f"{key}: Test {input_dict[key][1]}")
+            request = input("Enter mode: ")
+            valid_input = False
+            mode = None
+            while not valid_input:
+                try:
+                    mode = int(request)
+                    assert mode >= 0 and mode <= last
+                    valid_input = True
+                except:
+                    print(f"Invalid mode. Mode must be an integer between 0 and {last}")
+                    request = input("Enter mode: ")
+            if mode == 0:
+                close = True
+            else:
+                input_dict[mode][0]()
+
+    def run_codec(self, data, output=False):
+        encoded, dim, meta = self.codec.embed(data)
+        decoded = self.codec.extract(encoded).to_numpy_array()
+
+        match = (canon(encoded)-canon(decoded)<1e-8).all()
+
+        if output:
+            print("Embedding:")
+            print(f"{data}")
+            print("leads to:")
+            print(f"{output}")
+            print("Decoded:")
+            print(f"{decoded}")
+
+        return match, encoded, decoded
+        
+    def specific_array(self):
+        print("Input array in form [[a,b,c...],[..],[..]]:")
+        array = np.zeros(shape=(self.n, self.k))
+        array_str = list(input())
+        valid = False
+        while not valid:
+            try:
+                assert array_str.pop(0) == '['
+                assert array_str.pop() == ']'
+                for i in range(n):
+                    assert array_str.pop(0) == '['
+                
+                    for j in range(k):
+                        delim = ']' if j == k-1 else ','
+                        end = array_str.index(delim)
+                        array[i,j] = int(''.join(array_str[0:end]))
+                        array_str = array_str[end+1:]
+                    
+                    if i==n-1:
+                        assert not array_str
+                    else: 
+                        assert array_str.pop(0) == ','
+                valid = True
+
+            except AssertionError:
+                print("Invalid format - missing delimiter or array too large")
+                print("Input array in form [[a,b,c...],[..],[..]]:")
+                array_str = input()
+                
+            #except ValueError:
+                #print("Invalid format - number could not be parsed to array")
+                #print("Input array in form [[a,b,c...],[..],[..]]:")
+                #array_str = input()
+
+        self.run_codec(array, output=True)            
+
+    def random_arrays(self):
+        low_bound = float(input("Min value of array elements: "))
+        high_bound = float(input("Max value of array elements: "))
+        num = int(input("Number of arrays: "))
+
+        rng=np.random.default_rng()
+
+        arrays = [((high_bound-low_bound)*rng.random(size=(self.n, self.k))+low_bound) for i in range(num)]
+        results = [(data, self.run_codec(data)) for data in arrays]
+        filtered_results = [x for x in results if not x[1][0]]
+
+        print(f"{len(filtered_results)} mismatches in {num} runs.")
+        if input("Print mismatches? (y/n): ") == 'y':
+            print("Mismatches:")
+            print("_______________")
+            for result in filtered_results:
+                print("Data: ")
+                print(canon(result[0]))
+                print("Decoded: ")
+                print(canon(result[1][2]))
+                print("_______________")
+
+if __name__ == "__main__":
+    n = int(input("n: "))
+    k = int(input("k: "))
+    ui = UI(n,k)
+    ui.menu()
+
+        
+
+        
+            
+        
+        
+                    
+        
